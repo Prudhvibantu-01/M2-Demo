b13bdd7ae7fb9c3d4ed9dab43db6ccacc310a758
prudw <prudw@LAPTOP-2GPQQ2UP>
2026-03-12 11:25:40 +0530

    pushing new code

--- src/test/java/Dem01/DeMo2.java
+++ src/test/java/Dem01/DeMo2.java
@@ -4,7 +4,8 @@ public class DeMo2 {
 
 	public static void main(String[] args) {
 		System.out.println("new pratice pull");
-		System.out.println("new branch");
+	    System.out.println("this branch pull");
+		
 
 	}
 
